d4f0db035b612e433c8ae554a6c24be71023b2b4
Thibault Pouch <89460756+BoxOfPandor@users.noreply.github.com>
2024-02-21 11:17:20 +0100

    [~] add 2 function for verify if the valeur is between 0 and 20 & -2 and 20

--- stat.c
+++ stat.c
@@ -7,6 +7,31 @@
 
 #include <stdio.h>
 #include "include/orion.h"
+#include <stdbool.h>
+
+static bool are_stats_valid(stat_t stats)
+{
+    if (stats.str < 0 || stats.str > 20) return false;
+    if (stats.dex < 0 || stats.dex > 20) return false;
+    if (stats.con < 0 || stats.con > 20) return false;
+    if (stats.intell < 0 || stats.intell > 20) return false;
+    if (stats.cha < 0 || stats.cha > 20) return false;
+    if (stats.wis < 0 || stats.wis > 20) return false;
+
+    return true;
+}
+
+static bool are_modifs_valid(modif_t modif)
+{
+    if (modif.str < -20 || modif.str > 20) return false;
+    if (modif.dex < -20 || modif.dex > 20) return false;
+    if (modif.con < -20 || modif.con > 20) return false;
+    if (modif.intell < -20 || modif.intell > 20) return false;
+    if (modif.cha < -20 || modif.cha > 20) return false;
+    if (modif.wis < -20 || modif.wis > 20) return false;
+
+    return true;
+}
 
 stat_t get_stat_player(void)
 {
@@ -20,8 +45,9 @@ stat_t get_stat_player(void)
         fgets(input, 100, stdin);
         num = sscanf(input, "%d %d %d %d %d %d", &stats.str, &stats.dex, &stats.con,
                      &stats.intell, &stats.cha, &stats.wis);
-        if (num != 6) {
-            printf("Error: You must enter 6 values.\n");
+        if (num != 6 || !are_stats_valid(stats)) {
+            printf("Error: You must enter 6 values between 0 and 20.\n");
+            num = 0;
         }
     } while (num != 6);
     putchar('\n');
@@ -40,8 +66,9 @@ modif_t get_modif_player(void)
         fgets(input, 100, stdin);
         num = sscanf(input, "%d %d %d %d %d %d", &modif.str, &modif.dex, &modif.con,
                      &modif.intell, &modif.cha, &modif.wis);
-        if (num != 6) {
-            printf("Error: You must enter 6 values.\n");
+        if (num != 6 || !are_modifs_valid(modif)) {
+            printf("Error: You must enter 6 values between 0 and 20.\n");
+            num = 0;
         }
     } while (num != 6);
     putchar('\n');

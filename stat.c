d92b663a1a37d3018d4727ccd3910cdb7e02d72b
Thibault Pouch <89460756+BoxOfPandor@users.noreply.github.com>
2024-02-21 11:09:34 +0100

    [~] modify the way to give the information

--- stat.c
+++ stat.c
@@ -6,65 +6,57 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>
 #include "include/orion.h"
 
 stat_t get_stat_player(void)
 {
     stat_t stats;
+    char input[100];
+    int num;
 
-    printf("------------------------ Enter Stat ------------------------\n");
-    printf("Enter your stat of strength: ");
-    scanf("%d", &stats.str);
-    printf("Enter your stat of dexterity: ");
-    scanf("%d", &stats.dex);
-    printf("Enter your stat of constitution: ");
-    scanf("%d", &stats.con);
-    printf("Enter your stat of intelligence: ");
-    scanf("%d", &stats.intell);
-    printf("Enter your stat of charisma: ");
-    scanf("%d", &stats.cha);
-    printf("Enter your stat of wisdom: ");
-    scanf("%d", &stats.wis);
+    do {
+        printf("-------------------- Enter Stat --------------------\n");
+        printf("Enter your stats of str, dex, con, int, cha, wis:\n");
+        fgets(input, 100, stdin);
+        num = sscanf(input, "%d %d %d %d %d %d", &stats.str, &stats.dex, &stats.con,
+                     &stats.intell, &stats.cha, &stats.wis);
+        if (num != 6) {
+            printf("Error: You must enter 6 values.\n");
+        }
+    } while (num != 6);
+    putchar('\n');
     return stats;
 }
 
 modif_t get_modif_player(void)
 {
     modif_t modif;
-    char input[10];
+    char input[100];
+    int num;
 
-    printf("------------------------ Enter Modif ------------------------\n");
-    printf("Enter your modif of strength: ");
-    scanf("%s", input);
-    modif.str = atoi(input);
-    printf("Enter your modif of dexterity: ");
-    scanf("%s", input);
-    modif.dex = atoi(input);
-    printf("Enter your modif of constitution: ");
-    scanf("%s", input);
-    modif.con = atoi(input);
-    printf("Enter your modif of intelligence: ");
-    scanf("%s", input);
-    modif.intell = atoi(input);
-    printf("Enter your modif of charisma: ");
-    scanf("%s", input);
-    modif.cha = atoi(input);
-    printf("Enter your modif of wisdom: ");
-    scanf("%s", input);
-    modif.wis = atoi(input);
+    do {
+        printf("-------------------- Enter Modif --------------------\n");
+        printf("Enter your modifs of str, dex, con, int, cha, wis:\n");
+        fgets(input, 100, stdin);
+        num = sscanf(input, "%d %d %d %d %d %d", &modif.str, &modif.dex, &modif.con,
+                     &modif.intell, &modif.cha, &modif.wis);
+        if (num != 6) {
+            printf("Error: You must enter 6 values.\n");
+        }
+    } while (num != 6);
+    putchar('\n');
     return modif;
 }
 
 void print_stat(stat_t stats, modif_t modif)
 {
-    printf("------|------------ Your Stat ------------|\n");
-    printf("      | Str | Dex | Con | Int | Cha | Win |\n");
-    printf("------|-----------------------------------|\n");
-    printf("Stat  | %3d | %3d | %3d | %3d | %3d | %3d |\n", stats.str, stats.dex,
+    printf("|-------|------------ Your Stat ------------|\n");
+    printf("|       | Str | Dex | Con | Int | Cha | Win |\n");
+    printf("|-------|-----|-----|-----|-----|-----|-----|\n");
+    printf("| Stats | %3d | %3d | %3d | %3d | %3d | %3d |\n", stats.str, stats.dex,
            stats.con, stats.intell, stats.cha, stats.wis);
-    printf("------|-----------------------------------|\n");
-    printf("Modif | %+3d | %+3d | %+3d | %+3d | %+3d | %+3d |\n", modif.str, modif.dex,
+    printf("|-------|-----|-----|-----|-----|-----|-----|\n");
+    printf("| Modif | %+3d | %+3d | %+3d | %+3d | %+3d | %+3d |\n", modif.str, modif.dex,
            modif.con, modif.intell, modif.cha, modif.wis);
-    printf("------|-----------------------------------|\n");
+    printf("|-------|-----------------------------------|\n");
 }
